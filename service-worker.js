6e2185e56696f3b4094d040c9b544c763efdb502
Daly-New-Dev <sreyneat07072000@gmail.com>
2025-07-04 09:48:54 +0700

    n

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // A more robust service worker with an update notification system.
 
-const CACHE_NAME = 'school-admin-cache-v23'; // **IMPORTANT**: Version incremented to trigger update.
+const CACHE_NAME = 'school-admin-cache-v24'; // **IMPORTANT**: Version incremented to trigger update.
 const urlsToCache = [
   './',
   './index.html',
